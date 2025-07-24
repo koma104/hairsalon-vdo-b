fb493b890d628b20ea608f3167ccd17a3638123a
ToshihitoKomatsu <komatsu@dsnf.co.jp>
2025-07-24 10:47:49 +0900

    feat: フッターにGoogle Mapsアクセスリンクを追加

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -13,6 +13,25 @@ const Footer = () => {
               <br />
               東京都渋谷区渋谷1-2-3 渋谷ビルディング103-A
             </p>
+            <a 
+              href="https://maps.google.com/?q=東京都渋谷区渋谷1-2-3+渋谷ビルディング103-A" 
+              target="_blank" 
+              rel="noopener noreferrer"
+              className={styles['access-link']}
+            >
+              <svg 
+                xmlns="http://www.w3.org/2000/svg" 
+                width="16" 
+                height="16" 
+                fill="currentColor" 
+                viewBox="0 0 16 16"
+                className={styles['access-icon']}
+              >
+                <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10"/>
+                <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+              </svg>
+              Google map
+            </a>
             <p>TEL: 03-1234-5678</p>
           </div>
           <div className={styles.hours}>
